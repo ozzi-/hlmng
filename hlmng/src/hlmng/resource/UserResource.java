3fc7d76a7cec619aa42b08b776ae764599b49738
ozzi <ozgheb@hsr.ch>
2015-03-30 14:29:53 +0200

    + Users can now change their own regID (rest/user({id}/changeregid)

--- hlmng/src/hlmng/resource/UserResource.java
+++ hlmng/src/hlmng/resource/UserResource.java
@@ -1,6 +1,7 @@
 package hlmng.resource;
 
 import hlmng.auth.AuthChecker;
+import hlmng.auth.AuthCredential;
 import hlmng.dao.GenDao;
 import hlmng.dao.GenDaoLoader;
 import hlmng.dao.UserDao;
@@ -9,6 +10,7 @@ import hlmng.model.UserActionLimiter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -45,7 +47,44 @@ public class  UserResource  extends Resource {
 	@Path("{id}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response putUser(User element,@PathParam("id") int id) throws IOException {
-		return putResource(userDao, element, id);			
+		return putResource(userDao, element, id);
+	}
+	
+	@PUT
+	@Path("{id}/changeregid")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response putUserChangeRegID(User element,@PathParam("id") int id) throws IOException {
+		Response res;
+		String authorizationHeader = headers.getHeaderString("Authorization");
+		if(authorizationHeader==null){
+			return Response.status(401).build();
+		}
+		if(element==null || element.getName()==null){
+			return Response.status(400).build();
+		} 
+				
+		AuthCredential authCredential = AuthChecker.decodeBasicAuthB64(authorizationHeader);
+		
+		// only allow the user to his own object
+		if( authCredential.getUsername().equals(element.getName()) &&  (AuthChecker. check(headers, servletResponse, false))){
+			User dbUser = (User)userDao.getElement(id);
+			if (dbUser!=null){
+				// only allow changes to regID since this is the user himself wanting to change stuff, don't trust him
+				element.setName(dbUser.getName());
+				element.setDeviceID(dbUser.getDeviceID());
+				element.setUserID(dbUser.getUserID());
+				userDao.updateElement(element, id);
+				res = Response.accepted().build();
+				Log.addEntry(Level.WARNING, "User ("+authCredential.getUsername()+") successfully changed his registration id");
+			}else{
+				Log.addEntry(Level.WARNING, "User ("+authCredential.getUsername()+") tried to edit a non existant user");
+				res = Response.status(404).build();
+			}
+		}else{
+			Log.addEntry(Level.WARNING, "User ("+authCredential.getUsername()+") tried to PUT user with wrong credentials or wrong name");
+			res = Response.status(401).build();
+		}
+		return res;		
 	}
 	
 	@DELETE
