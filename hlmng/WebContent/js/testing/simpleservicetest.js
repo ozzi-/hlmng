116758add00a9e2be1a0316f3910241206120c08
ozzi <ozgheb@hsr.ch>
2015-04-10 16:52:08 +0200

    + My first karma unit testing (sample test) + Karma Conf for project

---
+++ hlmng/WebContent/js/testing/simpleservicetest.js
@@ -0,0 +1,17 @@
+ 
+describe("SimpleService", function(){
+
+    beforeEach(module("app"));
+
+    var service;
+
+    beforeEach(inject(function(SimpleService){
+       service = SimpleService;
+    }));
+
+    describe("getData", function(){
+        it("should return an array of items", function(){
+            expect(service.getData()).toBeDefined();
+        });
+    });
+});
