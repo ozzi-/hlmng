55ea275dcac1b892469cf1a21134faa8f30c6ab6
ozzi <ozgheb@hsr.ch>
2015-04-20 13:25:43 +0200

    + Events are sorted into upcoming, running and finished

--- hlmng/WebContent/js/controller/event.js
+++ hlmng/WebContent/js/controller/event.js
@@ -27,8 +27,28 @@ app.controller('EventIdController', ['$http','$routeParams','RestService', funct
 
 app.controller('EventListController', ['$http','RestService', function($http,RestService){
 	var hlmng = this;
-	hlmng.events = [];
+	hlmng.eventsUpcoming = [];
+	hlmng.eventsRunning = [];
+	hlmng.eventsFinished = [];
+	
+	
 	RestService.list('event').then(function(data){
-		hlmng.events=data;
+		var curDate = new Date().setHours(0,0,0,0);
+        $.each(data, function(i, item){
+        	var itemStartDate = new Date(item.startDate).setHours(0,0,0,0);
+        	var itemEndDate = new Date(item.endDate).setHours(0,0,0,0);
+        	if(itemStartDate<=curDate && itemEndDate>=curDate){
+        		//alert("RUNNING: "+itemStartDate+" - "+itemEndDate+" CURR: "+curDate);
+        		hlmng.eventsRunning.push(item);
+        	}
+        	if(itemEndDate<curDate && itemStartDate<curDate){
+        		//alert("FINISHED: "+itemStartDate+" - "+itemEndDate+" CURR: "+curDate);
+        		hlmng.eventsFinished.push(item);
+        	}
+        	if(itemStartDate>curDate && itemEndDate>curDate){
+        		//alert("UPCOMING: "+itemStartDate+" - "+itemEndDate+" CURR: "+curDate);
+        		hlmng.eventsUpcoming.push(item);
+        	}
+        });	
 	});
 }]);
