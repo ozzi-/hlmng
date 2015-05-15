d275c62f0c339f1e5b8e3e432c35b59b1737de0d
ozzi <ozgheb@hsr.ch>
2015-05-15 11:27:00 +0200

    + Frontend: Slider is now called Social Slider to avoid confusion with the voting sliders + Frontend: Bugfix of Social Slider, now using socialID

--- hlmng/WebContent/frontend/js/socialslider_app.js
+++ hlmng/WebContent/frontend/js/socialslider_app.js
@@ -1,4 +1,4 @@
-var app = angular.module('hlmngSliderApp',['stateprovider','ui.bootstrap','ui.router']);
+var app = angular.module('hlmngSocialSliderApp',['stateprovider','ui.bootstrap','ui.router']);
 var apiUrl = '../rest/adm/';
 
 var myapp = angular.module('stateprovider', ["ui.router"]); 
@@ -7,8 +7,8 @@ myapp.config(function($stateProvider, $urlRouterProvider){
 	$stateProvider.
     state('event', {
 		url: "/event/{eventId:int}/interval/{interval:int}/updateinterval/{updateinterval:int}",
-		templateUrl: "template/slider/slider.html",
-       	controller: "SliderController",
+		templateUrl: "template/socialslider/socialslider.html",
+       	controller: "SocialSliderController",
        	controllerAs: "sliderCtrl"
     }).
     state('index', {
@@ -18,25 +18,27 @@ myapp.config(function($stateProvider, $urlRouterProvider){
 });
 
 
-app.controller('SliderController', ['$http','$stateParams','RestService','$scope','$interval',function($http,$stateParams,RestService,$scope,$interval){
+app.controller('SocialSliderController', ['$http','$log','$stateParams','RestService','$scope','$interval',function($http,$log,$stateParams,RestService,$scope,$interval){
 	
 	var refreshData = function() {
+		
 		RestService.list('social').then(function(data){
 		    $.each(data, function(i, item){
 		    	if(item.eventIDFK==$stateParams.eventId){
-		    		if(item.status=="accepted"){
-		    			var there=false;
-		    			for(var i=0; i<$scope.socialsAccepted.length; i++) {
-	    			        if ($scope.socialsAccepted[i].media == item.media){
-	    			        	there=true;
-	    			        }
-	    			    }
-		    			if(there==false){
-		    				$scope.socialsAccepted.push(item);	 
-		    			}
-		    		}else{
-		    			$scope.socialsAccepted.splice(item, 1);
-		    		}
+		    		var there=false;
+		    		var thereIndex=0;
+	    			for(var i=0; i<$scope.socialsAccepted.length; i++) {
+    			        if ($scope.socialsAccepted[i].socialID == item.socialID){
+    			        	there=true;
+    			        	thereIndex=i;
+    			        }
+    			    }
+	    			if(there==false && item.status=="accepted"){
+    					$scope.socialsAccepted.push(item);	
+	    			}
+	    			if(there==true && item.status!="accepted"){
+    					$scope.socialsAccepted.splice(thereIndex,1);	 
+	    			}
 		    	}
 		    });
 		});
@@ -51,13 +53,14 @@ app.controller('SliderController', ['$http','$stateParams','RestService','$scope
 	    }
 	});
 	
+	
 	$scope.socialsAccepted = [];
 	$scope.intervalSlide = $stateParams.interval;
 	RestService.list('social').then(function(data){
 	    $.each(data, function(i, item){
 	    	if(item.eventIDFK==$stateParams.eventId){
 	    		if(item.status=="accepted"){
-	    			$scope.socialsAccepted.push(item);	  	    			
+	    			$scope.socialsAccepted.push(item);
 	    		}
 	    	}
 	    });
